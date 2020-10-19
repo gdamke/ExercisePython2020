23bbf1c0c76a260d344372a6ab5ffc3f7cbccbc2
Guillermo Damke <gdamke@gmail.com>
2020-10-19 17:13:48 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Welcome to the Git (and Github) exercise
+
+Just some random text.
---
+++ programa.py
@@ -0,0 +1,19 @@
+#/usr/bin/env python
+
+import numpy as np
+
+def suma():
+    pass
+
+def resta():
+    pass
+    
+def multiplicacion():
+    pass
+
+def division():
+    pass
+
+def potencia():
+    pass
+
